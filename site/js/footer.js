0b3e568e85ef534b085dc4ac7164de409f273b48
ron <rontekhne@users.noreply.github.com>
2023-12-21 18:20:35 -0300

    updated privacy policies.

--- site/js/footer.js
+++ site/js/footer.js
@@ -3,7 +3,10 @@ function showFooter()
     let footer = 
     `
     <footer>
-        <p>Desenvolvido por Ron, 2024</p>
+        <p>©Copyright Rontekhne</p>
+        <p>RonTekhne é uma inciativa de Ron</p>
+        <p>Para a política de privacidade clique 
+        <a href="/site/src/privacy/rontekhne_privacy.html">aqui</a></p>
     </footer>
     `;
     document.write(footer);
