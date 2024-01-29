5ce93065dde393b631959a8fb8dea3c0857202fe
ron <ron@users.github.noreply>
2024-01-29 13:38:22 -0300

    added github link in the footer

--- site/js/footer.js
+++ site/js/footer.js
@@ -7,6 +7,7 @@ function showFooter()
         <p>RonTekhne é uma inciativa de Ron</p>
         <p>Para a política de privacidade clique 
         <a href="/site/src/privacy/rontekhne_privacy.html">aqui</a></p>
+        <img width="30" height="30" src="/site/img/github.png" alt="github icon">
     </footer>
     `;
     document.write(footer);
