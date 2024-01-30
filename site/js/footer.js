254308e41b8c2ef94fe2546464a3e3c81ae7cee7
ron <ron@users.github.noreply>
2024-01-29 23:39:43 -0300

    Fix: github icon

--- site/js/footer.js
+++ site/js/footer.js
@@ -7,7 +7,7 @@ function showFooter()
         <p>RonTekhne é uma inciativa de Ron</p>
         <p>Para a política de privacidade clique 
         <a href="/site/src/privacy/rontekhne_privacy.html">aqui</a></p>
-        <a class="alink-clean" href="https://github.com/rontekhne" target="_blank"><img src="/site/img/github.png" alt="github icon"></a>
+        <a class="alink-clean" href="https://github.com/rontekhne" target="_blank"><img width="20" height="20" src="/site/img/github.png" alt="github icon"></a>
     </footer>
     `;
     document.write(footer);
