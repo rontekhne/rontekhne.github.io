8e1bd73c5104de6a8cda936dc8411127f4afc7ca
ron <ron@users.github.noreply>
2024-04-11 18:12:40 -0300

    update post images

--- site/js/posts.js
+++ site/js/posts.js
@@ -3,17 +3,8 @@ function showPosts()
     let posts = 
     `
     <section>
-        <!--article>
-            <h2>CX3</h2>
-            <p> 
-                O CX3 é um curso introdutório de programação em C com 
-                o intuito de ensinar a programação de computadores, 
-                estruturas de dados, algoritmos e desenvolvimento de jogos
-                com qualidade e de forma gratuita!
-            </p>
-            <a class="read" href="/post/04-02-2024_cx3.html">LER</a>
-        </article-->
         <article>
+        <img class="post-card-img" src="https://raw.githubusercontent.com/rontekhne/OitoP/main/app/src/main/res/mipmap-hdpi/oitop.png" width="100" height="100" alt="8p logo">
             <h2>OITOP</h2>
             <p> 
                 O quebra-cabeças de 8 peças, como o nome diz, possui 8 peças, 
@@ -24,6 +15,7 @@ function showPosts()
             <a class="read" href="/post/02-01-2024_oitop.html">LER</a>
         </article>
         <article>
+        <img class="post-card-img" src="https://raw.githubusercontent.com/rontekhne/Arx/main/app/src/main/res/mipmap-hdpi/arx_icon.png" width="100" height="100" alt="Arx logo">
             <h2>ARX ARCADE 2D</h2>
             <p> 
                 Os arxes roubaram as almas da galáxia. Você como 
@@ -34,6 +26,7 @@ function showPosts()
             <a class="read" href="/post/23-09-2023_arx.html">LER</a>
         </article>
         <article>
+        <img class="post-card-img" src="https://raw.githubusercontent.com/rontekhne/rontekhne.github.io/main/site/img/R.png" width="100" height="100" alt="RonTekhne logo">
             <h2>RONTEKHNE PARA ANDROID</h2>
             <p>
                 O blog está no Android. Oba! Leve o  RonTekhne 
@@ -44,6 +37,7 @@ function showPosts()
             <a class="read" href="/post/11-09-2023_rontekhne_para_android.html">LER</a>
         </article>
         <article>
+        <img class="post-card-img" src="../site/img/tree.png" width="100" height="100" alt="Árvore">
             <h2>DIGA OLÁ!</h2>
             <p>
                 Nesta primeira postagem o Ron explica o motivo 
